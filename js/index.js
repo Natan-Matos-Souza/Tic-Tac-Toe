a1e7be52d741a125f956f8ff2aef173f70e80912
Natan Matos <natan>
2023-04-19 15:01:09 -0300

    criando estruturas arquivos

---
+++ js/index.js
@@ -0,0 +1,13 @@
+//Importing elements
+
+
+const startgame = document.getElementById("start-game-btn")
+const startgamedisplay = document.getElementById("start-game")
+
+startgame.onclick = hideFunction = () => {
+
+    startgamedisplay.style.display = "none"
+
+
+
+}
