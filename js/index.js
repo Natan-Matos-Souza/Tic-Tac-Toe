4bfe428ee5bb8da9d431d2e18529ede21d4cb25f
Natan Matos <natanmatos_18@hotmail.com>
2023-04-20 13:54:27 -0300

    Construindo estruturas

--- js/index.js
+++ js/index.js
@@ -11,9 +11,37 @@ startgame.onclick = hideStartGame = () => {
     startgamedisplay.style.animation = "dissapearEffect 2s"
     setTimeout(disspear = () => {
         startgamedisplay.style.display = "none"
-        testtext.style.animation = "appearEffect 1s"
+        testtext.style.animation = "appearEffect 500ms"
         testtext.style.display = "block"
     }, 300)
     
 }
 
+const xbtn = document.getElementById('X-button')
+const obtn = document.getElementById('O-button')
+const gameConfigurationHud = document.getElementById('game-configuration')
+
+
+
+xbtn.onclick = selectX = () => {
+
+    var playerOneCharacter = "X"
+    gameConfigurationHud.style.animation = "dissapearEffect 2s"
+    setTimeout(dissapear = () => {
+        gameConfigurationHud.style.display = "none"
+    }, 300)
+    
+
+}
+
+obtn.onclick = selectO = () => {
+
+    var playerOneCharacter = "O"
+    gameConfigurationHud.style.animation = "dissapearEffect 2s"
+    setTimeout(dissapear = () => {
+
+        gameConfigurationHud.style.display = "none"
+
+    }, 300)
+
+}
